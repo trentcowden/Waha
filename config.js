95ba58cb2a35f08d3d048620d093d067ed69fcad
trentcowden <37824478+trentcowden@users.noreply.github.com>
2020-02-26 14:52:44 -0800

    added config file for firebase

---
+++ config.js
@@ -0,0 +1,15 @@
+import Firebase from 'firebase';
+
+let config = {
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***;
+
+let app = Firebase.initializeApp(config);
+export const db = app.database();
