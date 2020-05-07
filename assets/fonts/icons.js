dad25db907458b5eab8c6f99e7d090e22a41f336
trentcowden <37824478+trentcowden@users.noreply.github.com>
2020-05-07 12:50:09 -0700

    updated icons

--- assets/fonts/icons.js
+++ assets/fonts/icons.js
@@ -1,118 +1,62 @@
 import { createIconSet } from 'react-native-vector-icons'
 const glyphMap = {
   'group-add': '\uea01',
-
-  'archive': '\uea02',
-
+  archive: '\uea02',
   'arrow-left': '\uea03',
-
   'arrow-right': '\uea04',
-
-  'cancel': '\uea05',
-
-  'check': '\uea06',
-
+  cancel: '\uea05',
+  check: '\uea06',
   'check-filled': '\uea07',
-
   'check-outline': '\uea08',
-
   'cloud-check-outline': '\uea09',
-
   'cloud-download-outline': '\uea0a',
-
-  'email': '\uea0b',
-
-  'group': '\uea0c',
-
-  'camera': '\uea0d',
-
+  email: '\uea0b',
+  group: '\uea0c',
+  camera: '\uea0d',
   '1-filled': '\uea0e',
-
   '1-outline': '\uea0f',
-
   '2-filled': '\uea10',
-
   '2-outline': '\uea11',
-
   '3-filled': '\uea12',
-
   '3-outline': '\uea13',
-
-  'pencil': '\uea14',
-
+  pencil: '\uea14',
   'triangle-right': '\uea15',
-
   'playlist-add': '\uea16',
-
   'playlist-check': '\uea17',
-
   'minus-filled': '\uea18',
-
-  'security': '\uea19',
-
+  security: '\uea19',
   'share-android': '\uea1a',
-
-  'storage': '\uea1b',
-
+  storage: '\uea1b',
   'plus-filled': '\uea1c',
-
-  'announcement': '\uea1d',
-
-  'backspace': '\uea1e',
-
-  'camera': '\uea1f',
-
+  announcement: '\uea1d',
+  backspace: '\uea1e',
+  camera: '\uea1f',
   'cloud-check-filled': '\uea20',
-
   'cloud-download-filled': '\uea21',
-
   'cloud-slash-outline': '\uea22',
-
-  'trash': '\uea23',
-
-  'description': '\uea24',
-
+  trash: '\uea23',
+  description: '\uea24',
   'error-filled': '\uea25',
-
   'error-outline': '\uea26',
-
-  'record': '\uea27',
-
+  record: '\uea27',
   'skip-forward': '\uea28',
-
   'fullscreen-enter': '\uea29',
-
   'fullscreen-exit': '\uea2a',
-
-  'help': '\uea2b',
-
-  'info': '\uea2c',
-
-  'language': '\uea2d',
-
-  'menu': '\uea2e',
-
-  'notes': '\uea2f',
-
+  help: '\uea2b',
+  info: '\uea2c',
+  language: '\uea2d',
+  menu: '\uea2e',
+  notes: '\uea2f',
   'quad-arrow': '\uea30',
-
   'pause-filled': '\uea31',
-
   'play-filled': '\uea32',
-
   'minus-outline': '\uea33',
-
   'skip-back': '\uea34',
-
-  'settings': '\uea35',
-
+  settings: '\uea35',
   'share-ios': '\uea36',
-
   'triangle-left': '\uea37',
-
-  'volume': '\uea38',
-
-  'key': '\uea39'
+  volume: '\uea38',
+  key: '\uea39'
 }
 export default Icon = createIconSet(
   glyphMap,
