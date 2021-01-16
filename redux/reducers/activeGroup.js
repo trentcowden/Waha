37f5b1065647a640a11baf62ecc280255cf215ba
trentcowden <37824478+trentcowden@users.noreply.github.com>
2021-01-15 16:36:57 -0800

    removing tajawal font

--- redux/reducers/activeGroup.js
+++ redux/reducers/activeGroup.js
@@ -1,5 +1,9 @@
 import { CHANGE_ACTIVE_GROUP } from '../actions/groupsActions'
 
+/**
+ * The active group redux reducer that stores the name of the currently active group.
+ * @param {*} action Parameters passed from
+ */
 export function activeGroup (state = null, action) {
   switch (action.type) {
     // note: only stores the active group's names
