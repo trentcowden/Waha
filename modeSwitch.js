56a1dcd1984a6c682439d45eda330d14392ed5f8
trentcowden <37824478+trentcowden@users.noreply.github.com>
2021-01-25 09:32:28 -0800

    switched to prod

--- modeSwitch.js
+++ modeSwitch.js
@@ -1,8 +1,8 @@
 // test uses the test database
-export const dbMode = 'test'
+export const dbMode = 'prod'
 
 // test means that data isn't stored between app restarts
 export const reduxMode = 'prod'
 
 // test means analytics aren't tracked
-export const analyticsMode = 'test'
+export const analyticsMode = 'prod'
