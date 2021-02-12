42f8de152620ca31006cf81dac384100a5bca646
trentcowden <37824478+trentcowden@users.noreply.github.com>
2021-02-12 11:07:55 -0800

    splitting out stuff from constants into separate files

---
+++ assets/languageT2S/languageT2S.js
@@ -0,0 +1,6 @@
+export const languageT2S = {
+  en: require('./en.mp3'),
+  ga: require('./ga.mp3'),
+  te: require('./en.mp3'),
+  tt: require('./en.mp3')
+***REMOVED***
---
+++ languages.js
@@ -0,0 +1,46 @@
+export const languages = [
+  {
+    i18nName: 'english',
+    languageCode: 'en',
+    font: 'Roboto',
+    isRTL: false,
+    data: [
+      {
+        nativeName: 'English',
+        wahaID: 'en',
+        i18nName: 'english',
+        logoSource:
+          'https://firebasestorage.googleapis.com/v0/b/waha-app-db.appspot.com/o/en%2Fother%2Fheader-v1.png?alt=media'
+      ***REMOVED***,
+      {
+        nativeName: 'Test Language',
+        wahaID: 'te',
+        i18nName: 'english',
+        logoSource:
+          'https://firebasestorage.googleapis.com/v0/b/waha-app-db.appspot.com/o/en%2Fother%2Fheader-v1.png?alt=media'
+      ***REMOVED***,
+      {
+        nativeName: 'Test Language 2',
+        wahaID: 'tt',
+        i18nName: 'english',
+        logoSource:
+          'https://firebasestorage.googleapis.com/v0/b/waha-app-db.appspot.com/o/en%2Fother%2Fheader-v1.png?alt=media'
+      ***REMOVED***
+    ]
+  ***REMOVED***,
+  {
+    i18nName: 'arabic',
+    languageCode: 'ar',
+    font: 'NotoSansArabic',
+    isRTL: true,
+    data: [
+      {
+        nativeName: 'الخليج العربي',
+        wahaID: 'ga',
+        i18nName: 'gulfArabic',
+        logoSource:
+          'https://firebasestorage.googleapis.com/v0/b/waha-app-db.appspot.com/o/ga%2Fother%2Fheader-v1.png?alt=media'
+      ***REMOVED***
+    ]
+  ***REMOVED***
+]
