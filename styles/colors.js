77a84534d88af14accbb560915cce91351ee65ce
trentcowden <37824478+trentcowden@users.noreply.github.com>
2021-02-12 11:07:55 -0800

    splitting out stuff from constants into separate files

---
+++ styles/colors.js
@@ -0,0 +1,41 @@
+export const colors = {
+  // normal text
+  shark: '#1D1E20',
+
+  // secondary text and icons
+  tuna: '#3A3C3F',
+
+  // grayed out stuff
+  chateau: '#9FA5AD',
+
+  // gray borders
+  oslo: '#828282',
+
+  geyser: '#DEE3E9',
+
+  // backgrounds, list items, borders
+  porcelain: '#EAEEF0',
+  athens: '#EFF2F4',
+  aquaHaze: '#f7f9fa',
+  white: '#FFFFFF',
+
+  // other colors
+  apple: '#60C239',
+  red: '#FF0800',
+  blue: '#2D9CDB'
+}
+
+export const keyColors = {
+  0: '#ffe119',
+  1: '#3cb44b',
+  2: '#4363d8',
+  3: '#911eb4',
+  4: '#aaffc3',
+  5: '#f032e6',
+  6: '#e6194B',
+  7: '#42d4f4',
+  8: '#469990',
+  9: '#bfef45',
+  10: '#dcbeff',
+  11: '#9A6324'
+}
