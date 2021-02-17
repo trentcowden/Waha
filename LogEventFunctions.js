03d9c8423c4121e252a4fc2c5570ce18d969210d
trentcowden <37824478+trentcowden@users.noreply.github.com>
2021-02-16 17:01:17 -0800

    documenting log event functions

--- LogEventFunctions.js
+++ LogEventFunctions.js
@@ -3,6 +3,16 @@ import * as StoreReview from 'expo-store-review'
 import { getSetInfo ***REMOVED*** from './constants'
 import { analyticsMode ***REMOVED*** from './modeSwitch'
 
+/**
+ * This file contains a bunch of functions that send log events to Firebase analytics. This allows us to keep track of useful information.
+ */
+
+/**
+ * Logs the installation of a new language instance.
+ * @export
+ * @param {string***REMOVED*** languageName - The ID of the language instance.
+ * @param {string***REMOVED*** phoneLanguage - The ID of the phone language.
+ */
 export async function logInstallLanguage (languageName, phoneLanguage) {
   if (analyticsMode !== 'test')
     await Analytics.logEvent('InstallLanguage', {
@@ -11,7 +21,15 @@ export async function logInstallLanguage (languageName, phoneLanguage) {
     ***REMOVED***)
 ***REMOVED***
 
+/**
+ * Logs the completion of a lesson.
+ * @export
+ * @param {Object***REMOVED*** lesson - The lesson object of the lesson that was completed.
+ * @param {Object***REMOVED*** set - The set that the completed lesson is a part of.
+ * @param {string***REMOVED*** currentLanguage - The ID of the active group's language.
+ */
 export async function logCompleteLesson (lesson, set, currentLanguage) {
+  // If the lesson that is completed is the very first one, then request that the user submit a review on the App Store/Play Store.
   if (lesson.id.includes('1.1.1') && lesson.id.length === 8) {
     StoreReview.requestReview()
   ***REMOVED***
@@ -24,6 +42,12 @@ export async function logCompleteLesson (lesson, set, currentLanguage) {
     ***REMOVED***)
 ***REMOVED***
 
+/**
+ * Logs the completion of a Story Set.
+ * @export
+ * @param {Object***REMOVED*** set - The set object for the set that was completed.
+ * @param {string***REMOVED*** currentLanguage - The ID of the active group's language.
+ */
 export async function logCompleteStorySet (set, currentLanguage) {
   StoreReview.requestReview()
   if (analyticsMode !== 'test')
@@ -33,24 +57,48 @@ export async function logCompleteStorySet (set, currentLanguage) {
       language: currentLanguage
     ***REMOVED***)
 ***REMOVED***
+
+/**
+ * Logs the global unlock of the Mobilization Tools.
+ * @export
+ * @param {string***REMOVED*** currentLanguage - The ID of the active group's language.
+ */
 export async function logUnlockMobilizationTools (currentLanguage) {
   if (analyticsMode !== 'test')
     await Analytics.logEvent('UnlockMobilizationTools', {
       language: currentLanguage
     ***REMOVED***)
 ***REMOVED***
+
+/**
+ * Logs the creation of a new group.
+ * @export
+ * @param {string***REMOVED*** currentLanguage - The ID of the active group's language.
+ */
 export async function logCreateGroup (currentLanguage) {
   if (analyticsMode !== 'test')
     await Analytics.logEvent('CreateGroup', {
       language: currentLanguage
     ***REMOVED***)
 ***REMOVED***
+
+/**
+ * Logs enabling the Mobilization Tools for a specific group.
+ * @export
+ * @param {string***REMOVED*** currentLanguage - The ID of the active group's language.
+ */
 export async function logEnableMobilizationToolsForAGroup (currentLanguage) {
   if (analyticsMode !== 'test')
     await Analytics.logEvent('EnableMobilizationToolsForAGroup', {
       currentLanguage: currentLanguage
     ***REMOVED***)
 ***REMOVED***
+
+/**
+ * Logs adding a new story set.
+ * @export
+ * @param {Object***REMOVED*** set - The set object for the set that was added.
+ */
 export async function logAddStorySet (set) {
   if (analyticsMode !== 'test')
     await Analytics.logEvent('AddStorySet', {
@@ -58,18 +106,36 @@ export async function logAddStorySet (set) {
       setCategory: getSetInfo('category', set.id)
     ***REMOVED***)
 ***REMOVED***
+
+/**
+ * Logs sharing the app URL.
+ * @export
+ * @param {Object***REMOVED*** lesson - The lesson object for the lesson the user is doing when they share the app.
+ */
 export async function logShareApp (lesson) {
   if (analyticsMode !== 'test')
     await Analytics.logEvent('ShareApp', {
       lessonID: lesson.id
     ***REMOVED***)
 ***REMOVED***
+
+/**
+ * Logs sharing the scripture text for a lesson.
+ * @export
+ * @param {Object***REMOVED*** lesson - The lesson object for the lesson the user is doing when they share the scripture text.
+ */
 export async function logShareText (lesson) {
   if (analyticsMode !== 'test')
     await Analytics.logEvent('ShareText', {
       lessonID: lesson.id
     ***REMOVED***)
 ***REMOVED***
+
+/**
+ * Logs sharing the app Story chapter mp3.
+ * @export
+ * @param {Object***REMOVED*** lesson - The lesson object for the lesson the user is doing when they share the Story chapter mp3.
+ */
 export async function logShareAudio (lesson) {
   if (analyticsMode !== 'test')
     await Analytics.logEvent('ShareAudio', {
--- constants.js
+++ constants.js
@@ -12,10 +12,12 @@ const fontScale =
   PixelRatio.getFontScale() >= 1.2 ? 1.2 : PixelRatio.getFontScale()
 
 /** If the font scaling is larger than 1, we need to increase the heights of a few components to be able to fit the larger text. This variable acts as a multiplier for the height of those components. */
-
 const heightScaleModifier = 1 + (fontScale - 1) / 2
 
-/** This object stores the heights of a few specific components in Waha that react particularly badly to large font scaling. We need to declare the height of these dynamically, otherwise the text would never fit between a variety of fonts and font scaling. We have separate heights for each font in Waha as well as adding the heightScaleModifier we declared earlier. */
+/**
+ * This object stores the heights of a few specific components in Waha that react particularly badly to large font scaling. We need to declare the height of these dynamically, otherwise the text would never fit between a variety of fonts and font scaling. We have separate heights for each font in Waha as well as adding the heightScaleModifier we declared earlier.
+ * @export
+ * */
 export const itemHeights = {
   Roboto: {
     LessonItem: 60 * heightScaleModifier,
@@ -27,7 +29,10 @@ export const itemHeights = {
   ***REMOVED***
 ***REMOVED***
 
-/** This variable stores a multiplier value that we use to scale the dimensions of components throughout the app. It's based on the size of the phone screen. Basically, it shrinks components on smaller phones so that we're able to fit everything we need to on the screen. */
+/**
+ * This variable stores a multiplier value that we use to scale the dimensions of components throughout the app. It's based on the size of the phone screen. Basically, it shrinks components on smaller phones so that we're able to fit everything we need to on the screen.
+ * @export
+ * */
 export const scaleMultiplier =
   Dimensions.get('window').width > 400
     ? 1
