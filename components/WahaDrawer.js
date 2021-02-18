1d32d06f1c8d1dee71b7bb08453fd3c31dc120c3
trentcowden <37824478+trentcowden@users.noreply.github.com>
2021-02-18 15:18:22 -0800

    Version 1.0.5

--- components/WahaDrawer.js
+++ components/WahaDrawer.js
@@ -129,7 +129,7 @@ function WahaDrawer (props) {
                 colors.chateau
               )}
             >
-              v1.0.4
+              v1.0.5
             </Text>
           </View>
         </SafeAreaView>
