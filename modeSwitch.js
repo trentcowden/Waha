f40b6b851bc14611405506be18efa685f0704bac
trentcowden <37824478+trentcowden@users.noreply.github.com>
2021-06-15 17:03:58 -0700

    redux mode to prod

--- modeSwitch.js
+++ modeSwitch.js
@@ -2,12 +2,12 @@
 export const dbMode = 'test'
 
 // Determines what mode redux persistence is in. 'test' means that no redux data is persisted across app restarts and should be used only for testing. 'prod' means that some data is persisted across restarts. The app should always be build in 'prod' mode.
-export const reduxMode = 'test'
+export const reduxMode = 'prod'
 
 // Determines whether analytics are tracked. 'test' means that they aren't and 'prod' means that they are.
 export const analyticsMode = 'prod'
 
 // The current version of Waha. This is used to compare versions with the database and is also displayed in the navigation drawer.
-export const appVersion = '2.0.1'
+export const appVersion = '2.0.2'
 
 export const storageMode = 'prod'
