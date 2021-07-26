210d377a54edd0fb95f5cc60e2316b22d729c38c
trentcowden <37824478+trentcowden@users.noreply.github.com>
2021-07-26 11:27:36 -0700

    brainstorming new review system

--- redux/actions/persistedPopupsActions.js
+++ redux/actions/persistedPopupsActions.js
@@ -1,5 +1,10 @@
 export const SET_SHOW_TRAILER_HIGHLIGHTS = 'SET_SHOW_TRAILER_HIGHLIGHTS'
 
+// Complete lesson
+// 1. If conditions for review request are met, set reviewTimer to 1 hour
+// 2. In app state listener, if we're active and time is more than 1 hour ago, show review request
+// 3. Set reviewTimer to null
+
 export function setShowTrailerHighlights (toSet) {
   return {
     type: SET_SHOW_TRAILER_HIGHLIGHTS,
