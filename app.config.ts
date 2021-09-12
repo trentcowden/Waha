a5d67db9df81e018650d107c7f151b4ba43c45f7
trentcowden <37824478+trentcowden@users.noreply.github.com>
2021-09-12 09:13:42 -0700

    random config stuff

--- app.config.ts
+++ app.config.ts
@@ -1,13 +1,15 @@
-import { ExpoConfig, ConfigContext } from '@expo/config';
+import { ConfigContext, ExpoConfig } from '@expo/config'
 
-export default (context: ConfigContext) : ExpoConfig => {
-  const bundledLanguages = process.env.BUNDLED_LANGUAGES ? process.env.BUNDLED_LANGUAGES.split(',') : undefined;
+export default (context: ConfigContext): ExpoConfig => {
+  const bundledLanguages = process.env.BUNDLED_LANGUAGES
+    ? process.env.BUNDLED_LANGUAGES.split(',')
+    : undefined
   return {
     ...context.config,
-    "name": "Waha",
-    "slug": "Waha",
+    name: 'Waha',
+    slug: 'Waha',
     extra: {
       bundledLanguages
     }
-  };
+  }
 }
